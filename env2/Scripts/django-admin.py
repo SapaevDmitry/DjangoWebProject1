af7c0e9bca77996c26722c3d6dffcae87381a805
SapaevDmitry <dimkasapaevv@yandex.ru>
2023-05-03 11:59:59 +0300

    init

---
+++ env2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\веб 8\WEB course\DjangoWebProject1\DjangoWebProject1\env2\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
